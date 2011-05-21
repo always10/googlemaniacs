08eecc6553b704d017ae3766e7bd2d17986457ab
always10 <kdonaiski@gmail.com>
2011-05-21 14:28:45 +0200

    views

---
+++ views/Lubelskie/map.js
@@ -0,0 +1,10 @@
+function(doc) {
+  var i;
+  for(i in doc.Placemark)
+  {
+        if(doc.Placemark[i].AddressDetails.Country.CountryName == "Polska" && doc.Placemark[i].AddressDetails.Country.AdministrativeArea.AdministrativeAreaName =="Lubelskie")
+{
+  emit(doc.Placemark[i].address, [doc.Placemark[i].Point.coordinates[0],doc.Placemark[i].Point.coordinates[1]]);
+}
+  }
+}

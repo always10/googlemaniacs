f1a3aa7ce6385d6ffcfcb892d32800e8a3e3bb13
always10 <kdonaiski@gmail.com>
2011-05-30 08:02:52 +0200

    update viewsow

--- views/Odleglosc/Kujawsko-Pomorskie/map_reduce.js
+++ views/Odleglosc/Kujawsko-Pomorskie/map_reduce.js
@@ -1,30 +1,3 @@
-function(doc) {
-var i;
-//Torun
-var XX = 18.5984437;
-var YY = 53.0137902;
-var dist;
-for(i in doc.Placemark)
-    {
-        if(doc.Placemark[i].AddressDetails.Country.CountryName == "Polska" && doc.Placemark[i].AddressDetails.Country.AdministrativeArea.AdministrativeAreaName =="Kujawsko-Pomorskie")
-            {
-                var x = doc.Placemark[i].Point.coordinates[0];
-                var y = doc.Placemark[i].Point.coordinates[1];
-                var radlat1 = Math.PI * y/180;
-                var radlat2 = Math.PI * YY/180;
-                var radlon1 = Math.PI * x/180;
-                var radlon2 = Math.PI * XX/180;
-                var theta = x-XX;
-                var radtheta = Math.PI * theta/180;
-                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-                dist = Math.acos(dist);
-                dist = dist * 180/Math.PI;
-                dist = dist * 60 * 1.1515;
-                dist = dist * 1.609344;
-                emit([x,y,XX,YY],dist);
-            }
-    }
-}
 function(keys,values)
 {
 	var max = 0;
